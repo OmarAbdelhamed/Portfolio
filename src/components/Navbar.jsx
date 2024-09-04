4d8986539d1ab583a81fbd1912caa577f2bf871d
omar <omar792001@gmail.com>
2024-09-04 21:54:15 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-16 h-16 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             OA&nbsp;
-            <span className='sm:block hidden'>| Omar Ahmed</span>
+            <span className='sm:block'>| Omar Ahmed</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -51,7 +51,6 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      {' '}
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}

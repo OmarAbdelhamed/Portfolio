56fb62bf882390bb61c764da3e794deef9f0f3b5
omar <omar792001@gmail.com>
2024-09-06 18:04:40 +0300

    .

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,20 +1,22 @@
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Float, OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { CanvasLoader } from '..';
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <>
-      <hemisphereLight intensity={2} groundColor='black' />
-      <pointLight intensity={3} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75}
-        position={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
+      <Float speed={1} rotationIntensity={1} floatIntensity={1}>
+        <hemisphereLight intensity={2} groundColor='black' />
+        <pointLight intensity={3} />
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.5 : 0.75}
+          position={isMobile ? [0, -2, -1.5] : [0, -2, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </Float>
     </>
   );
 };
@@ -43,6 +45,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          enablePan={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />

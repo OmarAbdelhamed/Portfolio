dcc177408d7010863eeed0cd6dfc81335c5d8a11
omar <omar792001@gmail.com>
2024-09-06 17:39:43 +0300

    .

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,23 @@
 import { BallCanvas } from './canvas';
-import SectionWrapper  from '../hoc';
+import SectionWrapper from '../hoc';
 import { technologies } from '../constants';
+import { styles } from '../styles';
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <p className={`${styles.sectionSubText} text-center`}>
+        Some of What i know
+      </p>
+      <h2 className={`${styles.sectionHeadText} text-center mb-[50px]`}>My Skills</h2>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 

fcda9ff58b8f128b046d299d705a1f40a0aa0b0a
omar <omar792001@gmail.com>
2024-09-20 23:50:45 +0300

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about' aria-label="About" >
+        <a href='#about' aria-label='About Section Scroll Link'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{ y: [0, 24, 0] }}
